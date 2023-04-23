5504c7cd4a95c83bb32bfbf9d9505dd169b95053
vasiliy19-12-1997 <Wasyok.ru@yandex.ru>
2023-04-23 10:06:01 +0500

    1

--- src/Components/UI/Navbvar/navbar.tsx
+++ src/Components/UI/Navbvar/navbar.tsx
@@ -35,9 +35,6 @@ const Navbar = () => {
             <li>
               <Link to={"/shop"}>Shop</Link>
             </li>
-            <li>
-              <Link to={"/contact"}>Contact</Link>
-            </li>
           </ul>
         </nav>
       </div>
